e705c183f28b7040ef6e7898ffb63ae0ccc8c40e
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-06 18:50:37 +0530

    making accounsService instance to be available application wide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
+import { AppComponent } from "./app.component";
+import { AccountComponent } from "./account/account.component";
+import { NewAccountComponent } from "./new-account/new-account.component";
+import { AccountsService } from "./accounts.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [AccountsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
