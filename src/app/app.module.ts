b00b236184e71144167e380d8346df0c252cf076
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-06 19:53:05 +0530

    fixed a bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from "./app.component";
 import { AccountComponent } from "./account/account.component";
 import { NewAccountComponent } from "./new-account/new-account.component";
 import { AccountsService } from "./accounts.service";
+import { LoggingService } from "./logging.service";
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [AccountsService],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
